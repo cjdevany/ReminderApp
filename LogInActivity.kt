9791e43b39d9663ada54a46242ad528e5bcbd770
Jrhoads32 <62158166+Jrhoads32@users.noreply.github.com>
2021-04-10 16:08:36 -0600

    Add files via upload

---
+++ LogInActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+
+
+
+    }
+}
