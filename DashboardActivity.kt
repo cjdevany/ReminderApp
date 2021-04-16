e281d75cfb4b7c9de6313391d83157e528286c8e
Jrhoads32 <62158166+Jrhoads32@users.noreply.github.com>
2021-04-16 10:20:16 -0600

    Quicksort for Tasks List

--- DashboardActivity.kt
+++ DashboardActivity.kt
@@ -39,8 +39,11 @@ class DashboardActivity : AppCompatActivity() {
                         var task = document.data as MutableMap<String, Any?>
                         task["documentID"] = document.id
                         tasks.add(task)
+
                     }
                 }
+                sort(tasks, 0, tasks.size - 1)
+
                 //spawn in the listview and add the proper contents to it
                 var listView = findViewById<ListView>(R.id.dashboardListView)
                 var listAdapter = MyAdapter(tasks, this)
@@ -89,7 +92,7 @@ class DashboardActivity : AppCompatActivity() {
         //returns what to display in each cell depending on it's position on the screen
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             val tv = TextView(context)
-            tv.text = dataList[position]["name"] as String                            //display the proper index's value
+            tv.text = "${dataList[position]["name"]}  ${dataList[position]["deadline"]}"                            //display the proper index's value
             tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25F)    //changes font size
             tv.setPadding(10, 10, 10, 10)    //sets the padding
             return tv
@@ -104,4 +107,115 @@ class DashboardActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun sort(arr: ArrayList<MutableMap<String, Any?>>, low: Int, high: Int)
+    {
+
+        if(low < high)
+        {
+            var index = partition(arr, low, high)
+
+            sort(arr, low, index)
+            sort(arr, index + 1, high)
+        }
+
+    }
+
+    private fun partition(arr: ArrayList<MutableMap<String, Any?>>, low: Int, high: Int): Int
+    {
+        var pivot = arr[low]
+        var pivotDateValue = getDateValue(pivot["deadline"].toString())
+        var i: Int = low
+        var j: Int = high
+
+        Log.w("***Date Value ", pivotDateValue.toString())
+
+        while(i < j)
+        {
+            while(getDateValue(arr[i]["deadline"].toString()) <= pivotDateValue && i < high)
+            {
+                i += 1
+            }
+
+            while(getDateValue(arr[j]["deadline"].toString()) > pivotDateValue && j > low)
+            {
+                j -= 1
+            }
+
+            if(i < j)
+            {
+                var swap = arr[i]
+                arr[i] = arr[j]
+                arr[j] = swap
+            }
+
+        }
+
+        arr[low] = arr[j]
+        arr[j] = pivot
+
+        return j
+
+    }
+
+    private fun getDateValue(mm_dd_yyyy_time: String): Long {
+
+        //This will split the string to ['mm', 'dd', 'yyyy hh:mm']
+        var dateSplit = mm_dd_yyyy_time.split("/").toTypedArray()
+
+        //Splits ['yyyy xx:xx'] to ['yyyy', 'hh:mm']
+        var yearSplit = dateSplit[2].split(" ").toTypedArray()
+
+        //Now lets split the time to hours and minutes: ['xx:xx'] to ['hh', 'mm']
+        var timeSplit = yearSplit[1].split(":").toTypedArray()
+
+        /**Then I want to parse each var to an int, this removes any leading 0's and keeps things consistent,
+        That way I don't add extra 0's later on.*/
+
+        var yearValue = yearSplit[0].toInt()
+        var monthValue = dateSplit[0].toInt()
+        var dayValue = dateSplit[1].toInt()
+        var hourValue = timeSplit[0].toInt()
+        var minuteValue = timeSplit[1].toInt()
+
+        /**
+         * For our "date value", want to create an Int that has a consistent length with
+         * all other date objects. This means that for any single digit, we need to add a 0 as well.
+         * Easier to just convert to a string and concat a '0'
+         *
+         * Formate for the value - yyyymmddhhmm
+         */
+
+        //Are the month, date, time values single-digits??
+        var month: String
+        var day: String
+        var hour: String
+        var minute: String
+
+        if(monthValue < 10)
+            month = "0$monthValue"
+        else
+            month ="$monthValue"
+
+        if(dayValue < 10)
+            day = "0$dayValue"
+        else
+            day ="$dayValue"
+
+        if(hourValue < 10)
+            hour = "0$hourValue"
+        else
+            hour ="$hourValue"
+
+        if(minuteValue < 10)
+            minute = "0$minuteValue"
+        else
+            minute ="$minuteValue"
+
+        //Now combine all the values into a single string!
+        var valueStr = "$yearValue$month$day$hour$minute"
+
+        return valueStr.toLong()
+    }
+
 }
